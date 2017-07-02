6fb80aef9cdc16e4cd16106af8f70ce528c7800e
Anton Österberg <anton.osterberg@gmail.com>
2017-07-02 17:22:54 +0200

    Refactoring resouceFinder: Split up functions in order to generalize and improve testability.

--- src/main/java/com/mantono/webserver/reflection/resourceFinder.kt
+++ src/main/java/com/mantono/webserver/reflection/resourceFinder.kt
@@ -1,7 +1,9 @@
 package com.mantono.webserver.reflection
 
 import com.mantono.webserver.ValidRequest
+import com.mantono.webserver.rest.ContentType
 import com.mantono.webserver.rest.Resource
+import com.mantono.webserver.rest.Verb
 import sun.misc.Request
 import java.io.File
 import java.lang.reflect.Method
@@ -10,11 +12,50 @@ import java.nio.file.Path
 import java.util.Arrays.stream
 import java.util.stream.Stream
 import kotlin.streams.asSequence
+import kotlin.streams.toList
 
 private val CLASS: Regex = Regex("\\w+\\.class\\b")
 val classPath: List<String> = System.getProperty("java.class.path").split(":".toRegex())
 
+val staticResources: Map<String, Path> by lazy {
+
+	val resourceRegex: Regex = Regex("/resources(/|$)")
+	val resourceDirs: List<String> = classPath.filter { resourceRegex.containsMatchIn(it) }
+
+	resourceDirs
+			.map { File(it).toPath() }
+			.map { Files.walk(it)}
+			.flatMap { it.toList() }
+			.map { withoutPath(it, resourceDirs) }
+			.filter { it.first.isNotEmpty() }
+			.toMap()
+}
+
+fun withoutPath(entirePath: Path, resourceDirs: List<String>): Pair<String, Path>
+{
+	val root: String = resourceDirs.firstOrNull{ it in entirePath.toString() }!!
+	val subPath: String = entirePath.toString().removePrefix(root)
+	return Pair(subPath, entirePath)
+}
+
 fun findResources(path: List<String> = classPath): Map<Resource, Method>
+{
+	return  methodsByAnnotation(Resource::class.java, path)
+			.filter { hasCorrectParameters(it) }
+			.map { pairOfResourceMethod(it) }
+			.asSequence()
+			.toMap()
+}
+
+fun <T : Annotation> methodsByAnnotation(annotation: Class<T>, path: List<String> = classPath): Stream<Method>
+{
+	return classesInPath(path)
+			.map { it.declaredMethods }
+			.flatMap(::stream)
+			.filter { it.isAnnotationPresent(annotation) }
+}
+
+fun classesInPath(path: List<String> = classPath): Stream<Class<*>>
 {
 	return  path.stream()
 			.parallel()
@@ -25,13 +66,6 @@ fun findResources(path: List<String> = classPath): Map<Resource, Method>
 			.filter { isClassFile(it.fileName)}
 			.map { asClass(it) }
 			.filterNull()
-			.map { it.declaredMethods }
-			.flatMap(::stream)
-			.filter { it.isAnnotationPresent(Resource::class.java) }
-			.filter { hasCorrectParameters(it) }
-			.map { pairOfResourceMethod(it) }
-			.asSequence()
-			.toMap()
 }
 
 fun pairOfResourceMethod(method: Method): Pair<Resource, Method>
